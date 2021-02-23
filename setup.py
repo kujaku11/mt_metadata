0373f072f57a134ba5b56c69d4755c9568e8d2cc
Karl N. Kappler <magnetotellurics@gmail.com>
2021-02-23 14:08:47 -0800

    add matplotlib to requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["numpy", "pandas", "pyyaml"]
+requirements = ["numpy", "pandas", "pyyaml", "matplotlib"]
 
 setup_requirements = [
     "pytest-runner",
