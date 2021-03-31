a78001ca6c76268be0d8daa59a06465f9d395449
JP <peacock.jared@gmail.com>
2021-03-31 15:55:04 -0700

    updated readme and updating tests

--- mt_metadata/utils/__init__.py
+++ mt_metadata/utils/__init__.py
@@ -4,7 +4,7 @@ from pathlib import Path
 # assume tests is on the root level of mt_metadata
 TEST_ROOT = Path(__file__).parent.parent.parent
 
-STATIONXML_01 = TEST_ROOT.joinpath("data/fdsn_no_mt_info.xml")
-STATIONXML_02 = TEST_ROOT.joinpath("data/mtml_single_station.xml")
-STATIONXML_MAGNETIC = TEST_ROOT.joinpath("data/mtml_magnetometer_example.xml")
-STATIONXML_ELECTRIC = TEST_ROOT.joinpath("data/mtml_electrode_example.xml")
+STATIONXML_01 = TEST_ROOT.joinpath("data/xml/fdsn_no_mt_info.xml")
+STATIONXML_02 = TEST_ROOT.joinpath("data/xml/mtml_single_station.xml")
+STATIONXML_MAGNETIC = TEST_ROOT.joinpath("data/xml/mtml_magnetometer_example.xml")
+STATIONXML_ELECTRIC = TEST_ROOT.joinpath("data/xml/mtml_electrode_example.xml")
