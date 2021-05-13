281a354bd1348cdc3b76829f9e1e0f9ec9c7c6cf
JP <peacock.jared@gmail.com>
2021-05-13 13:52:05 -0700

    updated where data is stored and added experiment folder

--- mt_metadata/utils/__init__.py
+++ mt_metadata/utils/__init__.py
@@ -4,7 +4,8 @@ from pathlib import Path
 # assume tests is on the root level of mt_metadata
 TEST_ROOT = Path(__file__).parent.parent.parent
 
-STATIONXML_01 = TEST_ROOT.joinpath("data/xml/fdsn_no_mt_info.xml")
-STATIONXML_02 = TEST_ROOT.joinpath("data/xml/mtml_single_station.xml")
-STATIONXML_MAGNETIC = TEST_ROOT.joinpath("data/xml/mtml_magnetometer_example.xml")
-STATIONXML_ELECTRIC = TEST_ROOT.joinpath("data/xml/mtml_electrode_example.xml")
+STATIONXML_01 = TEST_ROOT.joinpath("data/stationxml/fdsn_no_mt_info.xml")
+STATIONXML_02 = TEST_ROOT.joinpath("data/stationxml/mtml_single_station.xml")
+STATIONXML_MAGNETIC = TEST_ROOT.joinpath("data/stationxml/mtml_magnetometer_example.xml")
+STATIONXML_ELECTRIC = TEST_ROOT.joinpath("data/stationxml/mtml_electrode_example.xml")
+MT_EXPERIMENT_SINGLE_STATION = TEST_ROOT.joinpath("data/xml/single_station_mt_experiment.xml")
