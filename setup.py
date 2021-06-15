c20f9e4a0ca37b2799fdd2c76697a247b50d2fe7
Lindsey Heagy <lindseyheagy@gmail.com>
2021-06-15 08:06:03 -0700

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/kujaku11/mt_metadata",
-    version="0.1.1",
+    version="version='0.1.2'",
     zip_safe=False,
 )
