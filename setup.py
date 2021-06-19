f92348419362d6b9ba8819b9147044c0fd25a136
Lindsey Heagy <lindseyheagy@gmail.com>
2021-06-19 15:09:11 -0700

    simplify config for versioning and pypi upload

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/kujaku11/mt_metadata",
-    version="'0.1.3'",
+    version="0.1.3",
     zip_safe=False,
 )
