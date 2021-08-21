d10472bec84769e2bac4855f5278762d1cbc3183
JP <peacock.jared@gmail.com>
2021-08-20 17:47:53 -0700

    added data files cause forgot to

--- mt_metadata/utils/__init__.py
+++ mt_metadata/utils/__init__.py
@@ -1,17 +1,1 @@
 """Unit test package for mth5."""
-from pathlib import Path
-
-# assume tests is on the root level of mt_metadata
-TEST_ROOT = Path(__file__).absolute().parent.parent
-
-STATIONXML_01 = TEST_ROOT.joinpath("data/stationxml/fdsn_no_mt_info.xml")
-STATIONXML_02 = TEST_ROOT.joinpath("data/stationxml/mtml_single_station.xml")
-STATIONXML_MAGNETIC = TEST_ROOT.joinpath(
-    "data/stationxml/mtml_magnetometer_example.xml"
-)
-STATIONXML_ELECTRIC = TEST_ROOT.joinpath("data/stationxml/mtml_electrode_example.xml")
-MT_EXPERIMENT_SINGLE_STATION = TEST_ROOT.joinpath(
-    "data/mt_xml/single_station_mt_experiment.xml"
-)
-STATIONXML_FAP = TEST_ROOT.joinpath("data/stationxml/station_xml_with_fap_example.xml")
-STATIONXML_FIR = TEST_ROOT.joinpath("data/stationxml/station_xml_with_fir_example.xml")
