17f13f03ad34676838cddf4808b29baa33a2dc23
JP <peacock.jared@gmail.com>
2021-09-09 12:00:42 -0700

    updating writing emtf_xml got to data types

--- mt_metadata/transfer_functions/emtf_xml/data.py
+++ mt_metadata/transfer_functions/emtf_xml/data.py
@@ -5,6 +5,7 @@ Created on Mon Sep  6 13:53:55 2021
 @author: jpeacock
 """
 import numpy as np
+from xml.etree import cElementTree as et
 
 
 class TransferFunction:
@@ -14,7 +15,21 @@ class TransferFunction:
 
     def __init__(self):
         self.index_dict = {"hx": 0, "hy": 1, "ex": 0, "ey": 1, "hz": 0}
-        self.dtype_dict = {"complex": complex, "real": float}
+        self.dtype_dict = {
+            "complex": complex,
+            "real": float,
+            "complex128": "complex",
+            "float64": "real"}
+        self.units_dict = {"z": "[mV/km]/[nT]", "t": "[]"}
+        self.name_dict = {
+            "exhx": "zxx",
+            "exhy": "zxy",
+            "eyhx": "zyx",
+            "eyhy": "zyy",
+            "hzhx": "tx",
+            "hzhy": "ty",
+            }
+        
 
         self.periods = None
         self.z = None
@@ -35,6 +50,16 @@ class TransferFunction:
             "t_var": self.t_var,
             "t_invsigcov": self.t_invsigcov,
             "t_residcov": self.t_residcov,
+        }
+        self.write_dict = {
+            "z": {"out": {0: 'ex', 1: "ey"}, "in": {0: "hx", 1: "hy"}},
+            "z_var": {"out": {0: 'ex', 1: "ey"}, "in": {0: "hx", 1: "hy"}},
+            "z_invsigcov": {"out": {0: 'hx', 1: "hy"}, "in": {0: "hx", 1: "hy"}},
+            "z_residcov": {"out": {0: 'ex', 1: "ey"}, "in": {0: "ex", 1: "ey"}},
+            "t": {"out": {0: 'hz'}, "in": {0: "hx", 1: "hy"}},
+            "t_var": {"out": {0: 'hz'}, "in": {0: "hx", 1: "hy"}},
+            "t_invsigcov": {"out": {0: 'hx', 1: "hy"}, "in": {0: "hx", 1: "hy"}},
+            "t_residcov": {"out": {0: 'hz'}, "in": {0: "hz"}},
         }
 
     def initialize_arrays(self, n_periods):
@@ -108,3 +133,48 @@ class TransferFunction:
         for ii, block in enumerate(root_dict["data"]["period"]):
             self.periods[ii] = float(block["value"])
             self.read_block(block, ii)
+            
+    def write_block(self, parent, index):
+        """
+        Write a data block
+        
+        :param parent: DESCRIPTION
+        :type parent: TYPE
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+        
+        period_element = et.SubElement(
+            parent, "Period", {"value": self.period[index], "units":"secs"})
+        
+        for key in self.array_dict.keys():
+            arr = self.array_dict[key][index]
+            attr_dict = {
+                "type": self.dtype_dict[arr.dtype.name], 
+                "size": str(arr.shape)[1:-1].replace(',', '')
+                }
+            try:
+                attr_dict["units"] = self.units_dict[key]
+            except KeyError:
+                pass
+            
+            comp_element = et.SubElement(period_element, key, attr_dict)
+            idx_dict = self.write_dict[key]
+            for ii in arr.shape[0]:
+                for jj in arr.shape[1]:
+                    ch_out = idx_dict[ii]
+                    ch_in = idx_dict[jj]
+                    a_dict = {}
+                    try:
+                        a_dict["name"] = self.name_dict[ch_out + ch_in]
+                    except KeyError:
+                        
+                    comp_element = et.SubElement(
+                        comp_element,
+                        "value",
+                        attr_dict)
+                    
+                    
+        
+        
