fixing units bugs

mt_metadata/timeseries/filters/channel_response_filter.py
@@ -135,15 +135,19 @@ class ChannelResponseFilter(object):
                     continue
                 pb.append((f_pb.min(), f_pb.max()))
 
-        pb = np.array(pb)
-        return np.array([pb[:, 0].max(), pb[:, 1].min()])
+        if pb is not []:
+            pb = np.array(pb)
+            return np.array([pb[:, 0].max(), pb[:, 1].min()])
+        return None
 
     @property
     def normalization_frequency(self):
         """get normalization frequency from ZPK or FAP filter"""
 
         if self._normalization_frequency is None:
-            return np.round(self.pass_band.mean(), decimals=3)
+            if self.pass_band is not None:
+                return np.round(self.pass_band.mean(), decimals=3)
+            return None
 
         return self._normalization_frequency
 

mt_metadata/timeseries/filters/coefficient_filter.py
@@ -101,4 +101,4 @@ class CoefficientFilter(FilterBase):
         """
         if isinstance(frequencies, (float, int)):
             frequencies = np.array([frequencies])
-        return self.gain * np.ones(len(frequencies))
+        return self.gain * np.ones(len(frequencies), dtype=complex)

mt_metadata/timeseries/filters/pole_zero_filter.py
@@ -218,7 +218,7 @@ class PoleZeroFilter(FilterBase):
             if np.isclose(df_passband, df_0).all():
                 pass
             else:
-                self.logger.warning("Passband appears discontinuous")
+                self.logger.debug("Passband appears discontinuous")
         pass_band = np.array([pass_band.min(), pass_band.max()])
         return pass_band
 