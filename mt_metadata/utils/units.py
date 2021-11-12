ab2a15b6c10acecfcb476e529ddcf166baf9c3ee
JP <peacock.jared@gmail.com>
2021-11-12 14:22:50 -0800

    updated units.py

--- mt_metadata/utils/units.py
+++ mt_metadata/utils/units.py
@@ -4,7 +4,7 @@ This is a placeholder module.  See github issue #30
 In the mt_metadata packaage, the standard is that units are described by all
 lower case strings.
 
-The dictionaries (ABBREVIATIONS & PLOT_AXES_LABELS ) are keyed by these lower
+The dictionaries UNITS is keyed by these lower
 case strings.
 
 """
@@ -179,7 +179,7 @@ UNITS_LIST = [
         "description": "temperature",
         "abbreviation": "C",
         "plot_label": "Celsius",
-        "alias": ""
+        "alias": "celsius",
     },
 ]
 
@@ -223,49 +223,3 @@ def get_unit_object(unit, allow_none=True):
             f"Could not find {unit} in accetable units.  "
             "See mt_metadata.utils.units.py for more information")
    
-
-
-
-
-# ABBREVIATIONS = {
-#     "digital counts": "cts",
-#     "millivolts": "mV",
-#     "nanotesla": "nT",
-#     "tesla": "T",
-#     "volts": "V",
-#     "millivolt per kilometer": "mV/km",
-#     "millivolts per kilometer": "mV/km",
-#     "volt per meter": "V/m",
-#     "volts per meter": "V/m",
-#     "microvolt per meter": "\u03BCV/m",
-#     "microvolts per meter": "\u03BCV/m",
-#     "celsius": "C",
-# }
-
-# PLOT_AXES_LABELS = {
-#     "digital counts": "digital counts",
-#     "millivolts": "milliVolts",
-#     "tesla": "Tesla",
-#     "nanotesla": "nanoTesla",
-#     "volts": "Volts",
-# }
-
-# # =============================================================================
-# # Unit conversions
-# # =============================================================================
-# obspy_units_descriptions = {
-#     "T": "Tesla",
-#     "nT": "nanoTesla",
-#     "V": "Volts",
-#     "mV": "milliVolts",
-#     "count": "digital counts",
-#     "counts": "digital counts",
-#     "V/m": "Volts per meter",
-#     "mV/km": "milliVolts per kilometer",
-#     "m": "meters",
-#     "COUNTS": "digital counts",
-#     "millivolt per kilometer": "milliVolt per kilometer",
-#     "volt per meter": "Volt per meter",
-#     "volt": "Volt",
-#     "nanotesla": "nanoTesla",
-# }
