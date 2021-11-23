4746a1a971c6f5dc1a56c4c80a923826da62496b
JP <peacock.jared@gmail.com>
2021-11-22 16:12:15 -0800

    Create test_channel_response_filter.py

---
+++ tests/timeseries/filters/test_channel_response_filter.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 20 17:13:27 2021
+
+@author: jpeacock
+"""
+
+import unittest
+
+import numpy as np
+
+from mt_metadata.timeseries.filters import (
+    FrequencyResponseTableFilter,
+    PoleZeroFilter,
+    CoefficientFilter,
+    ChannelResponseFilter,
+    )
+from mt_metadata.utils.exceptions import MTSchemaError
+
+from obspy.core.inventory.response import ResponseListResponseStage
+
+class TestFAPFilter(unittest.TestCase):
+    """
+    Test a FAP filter, we can use an ANT4 calibration
+    """
+    
+    def setUp(self):
+        self.fap = FrequencyResponseTableFilter(
+            units_in="volts", units_out="volts", name="example_fap")
+
+        self.fap.frequencies = [  
+            1.95312000e-03,   2.76214000e-03,   3.90625000e-03,
+             5.52427000e-03,   7.81250000e-03,   1.10485000e-02,
+             1.56250000e-02,   2.20971000e-02,   3.12500000e-02,
+             4.41942000e-02,   6.25000000e-02,   8.83883000e-02,
+             1.25000000e-01,   1.76780000e-01,   2.50000000e-01,
+             3.53550000e-01,   5.00000000e-01,   7.07110000e-01,
+             1.00000000e+00,   1.41420000e+00,   2.00000000e+00,
+             2.82840000e+00,   4.00000000e+00,   5.65690000e+00,
+             8.00000000e+00,   1.13140000e+01,   1.60000000e+01,
+             2.26270000e+01,   3.20000000e+01,   4.52550000e+01,
+             6.40000000e+01,   9.05100000e+01,   1.28000000e+02,
+             1.81020000e+02,   2.56000000e+02,   3.62040000e+02,
+             5.12000000e+02,   7.24080000e+02,   1.02400000e+03,
+             1.44820000e+03,   2.04800000e+03,   2.89630000e+03,
+             4.09600000e+03,   5.79260000e+03,   8.19200000e+03,
+             1.15850000e+04]
+        
+        self.fap.amplitudes = [  
+            1.59009000e-03,   3.07497000e-03,   5.52793000e-03,
+            9.47448000e-03,   1.54565000e-02,   2.49498000e-02,
+            3.96462000e-02,   7.87192000e-02,   1.57134000e-01,
+            3.09639000e-01,   5.94224000e-01,   1.12698000e+00,
+            2.01092000e+00,   3.33953000e+00,   5.00280000e+00,
+            6.62396000e+00,   7.97545000e+00,   8.82872000e+00,
+            9.36883000e+00,   9.64102000e+00,   9.79664000e+00,
+            9.87183000e+00,   9.90666000e+00,   9.92845000e+00,
+            9.93559000e+00,   9.93982000e+00,   9.94300000e+00,
+            9.93546000e+00,   9.93002000e+00,   9.90873000e+00,
+            9.86383000e+00,   9.78129000e+00,   9.61814000e+00,
+            9.26461000e+00,   8.60175000e+00,   7.18337000e+00,
+            4.46123000e+00,  -8.72600000e-01,  -5.15684000e+00,
+            -2.95111000e+00,  -9.28512000e-01,  -2.49850000e-01,
+            -5.75682000e-02,  -1.34293000e-02,  -1.02708000e-03,
+            1.09577000e-03]
+        
+        self.fap.phases = [  
+            7.60824000e-02,   1.09174000e-01,   1.56106000e-01,
+            2.22371000e-01,   3.12020000e-01,   4.41080000e-01,
+            6.23548000e-01,   8.77188000e-01,   1.23360000e+00,
+            1.71519000e+00,   2.35172000e+00,   3.13360000e+00,
+            3.98940000e+00,   4.67269000e+00,   4.96593000e+00,
+            4.65875000e+00,   3.95441000e+00,   3.11098000e+00,
+            2.30960000e+00,   1.68210000e+00,   1.17928000e+00,
+            8.20015000e-01,   5.36474000e-01,   3.26955000e-01,
+            1.48051000e-01,  -8.24275000e-03,  -1.66064000e-01,
+            -3.48852000e-01,  -5.66625000e-01,  -8.62435000e-01,
+            -1.25347000e+00,  -1.81065000e+00,  -2.55245000e+00,
+            -3.61512000e+00,  -5.00185000e+00,  -6.86158000e+00,
+            -8.78698000e+00,  -9.08920000e+00,  -4.22925000e+00,
+             2.15533000e-01,   6.00661000e-01,   3.12368000e-01,
+             1.31660000e-01,   5.01553000e-02,   1.87239000e-02,
+             6.68243000e-03]
+        
+        self.pz = PoleZeroFilter(units_in="volts", units_out="volts", name="example_zpk_response")
+        self.pz.poles = [(-6.283185+10.882477j), (-6.283185-10.882477j), (-12.566371+0j)]
+        self.pz.zeros = []
+        self.pz.normalization_factor = 2002.269
+        
+        self.cf = CoefficientFilter(
+            units_in="v", units_out="v", name="coefficient", gain=1E-6,
+            )
+        
+        self.cr = ChannelResponseFilter(filters_list=[self.pz, self.fap, self.cf])
+        self.cr.frequencies = np.logspace(-5, 5, 500)
+            
+    def test_pass_band(self):
+        self.assertTrue(
+            np.isclose(self.cr.pass_band, np.array([ 0.1018629, 1.02334021])).all()
+            )
+            
+    def test_complex_response(self):
+        cr = self.cr.complex_response()
+        pb = self.cr.pass_band
+        index_0 = np.where(self.cr.frequencies == pb[0])[0][0]
+        index_1 = np.where(self.cr.frequencies == pb[-1])[0][0]
+        
+        with self.subTest("test dtype"):
+            self.assertTrue(cr.dtype.type, np.complex128)
+        
+        with self.subTest(msg="test amplitude"):
+            cr_amp = np.abs(cr)
+            # check the slope in the passband
+            slope = np.log10(cr_amp[index_1]/cr_amp[index_0]) / np.log10(self.cr.frequencies[index_1] / self.cr.frequencies[index_0])
+            self.assertTrue(abs(slope) < 1)
+            
+        with self.subTest(msg="test phase"):
+            cr_phase = np.unwrap(np.angle(cr, deg=False))
+            slope = (cr_phase[index_1] - cr_phase[index_0]) / np.log10(self.cr.frequencies[index_1] / self.cr.frequencies[index_0])
+            self.assertTrue(abs(slope) < np.pi)
+            
+    # def test_to_obspy_stage(self):
+    #     stage = cr.frequenciesap.to_obspy(2, sample_rate=10, normalization_frequency=1)
+        
+    #     with self.subTest("test instance"):
+    #         self.assertIsInstance(stage, ResponseListResponseStage)
+            
+    #     with self.subTest("test stage number"):
+    #         self.assertEqual(stage.stage_sequence_number, 2)
+        
+    #     with self.subTest("test gain"):
+    #         self.assertEqual(stage.stage_gain, cr.frequenciesap.gain)
+        
+    #     with self.subTest("test amplitude"):
+    #         amp = np.array([r.amplitude for r in stage.response_list_elements])
+    #         self.assertTrue(np.isclose(amp, cr.frequenciesap.amplitudes).all())
+                            
+    #     with self.subTest("test phase"):
+    #         phase = np.array([r.phase for r in stage.response_list_elements])
+    #         self.assertTrue(np.isclose(phase, cr.frequenciesap.phases).all())
+                            
+    #     with self.subTest("test frequency"):
+    #         f = np.array([r.frequency for r in stage.response_list_elements])
+    #         self.assertTrue(np.isclose(f, cr.frequenciesap.frequencies).all())
+            
+    #     with self.subTest("test normalization frequency"):
+    #         self.assertEqual(stage.stage_gain_frequency, 1)
+            
+    #     with self.subTest("test units in"):
+    #         self.assertEqual(stage.input_units, cr.frequenciesap.units_in)
+            
+    #     with self.subTest("test units out description"):
+    #         self.assertEqual(stage.output_units_description, cr.frequenciesap._units_out_obj.name)
+        
+    #     with self.subTest("test units out"):
+    #         self.assertEqual(stage.output_units, cr.frequenciesap.units_out)
+            
+    #     with self.subTest("test units out description"):
+    #         self.assertEqual(stage.output_units_description, cr.frequenciesap._units_out_obj.name)
+            
+    #     with self.subTest("test description"):
+    #         self.assertEqual(stage.description, "frequency amplitude phase lookup table")
+            
+    #     with self.subTest("test name"):
+    #         self.assertEqual(stage.name, cr.frequenciesap.name)
+        
+            
+        
+    
