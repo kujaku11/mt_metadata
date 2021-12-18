4e3de9806c96ce8dc0d93c05693018ea30b9dddc
JP <peacock.jared@gmail.com>
2021-12-17 16:56:04 -0800

    changed how emtfxml metadata is imported

--- mt_metadata/transfer_functions/io/emtfxml/emtfxml.py
+++ mt_metadata/transfer_functions/io/emtfxml/emtfxml.py
@@ -18,7 +18,7 @@ from collections import OrderedDict
 from xml.etree import cElementTree as et
 import numpy as np
 
-import .metadata as emtf_xml
+from . import metadata as emtf_xml
 from mt_metadata.utils.mt_logger import setup_logger
 from mt_metadata.base import helpers
 from mt_metadata.utils.validators import validate_attribute
