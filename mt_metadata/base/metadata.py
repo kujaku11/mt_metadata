bbc926537757559df9ec27c276c5402f1cc659fb
JP <peacock.jared@gmail.com>
2022-01-10 12:44:30 -0800

    updatind documentation

--- mt_metadata/base/metadata.py
+++ mt_metadata/base/metadata.py
@@ -13,7 +13,6 @@ Created on Wed Dec 23 20:41:16 2020
 # =============================================================================
 import json
 import pandas as pd
-import numpy as np
 import logging
 from copy import deepcopy
 
