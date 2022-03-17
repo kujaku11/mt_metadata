4bab335fe5b0da905a3f7276f6df2819ff843822
JP <peacock.jared@gmail.com>
2022-03-17 09:33:14 -0700

    removed superfalous standards and classes from TF and import those from TS

--- mt_metadata/transfer_functions/tf/station.py
+++ mt_metadata/transfer_functions/tf/station.py
@@ -14,6 +14,7 @@ Created on Wed Dec 23 21:30:36 2020
 from mt_metadata.base.helpers import write_lines
 from mt_metadata.base import get_schema, Base
 from .standards import SCHEMA_FN_PATHS
+from mt_metadata.timeseries.standards import SCHEMA_FN_PATHS as TS_SCHEMA_FN_PATHS
 from mt_metadata.utils.validators import validate_value_type
 from . import (
     Fdsn,
@@ -28,26 +29,26 @@ from . import (
 
 # =============================================================================
 attr_dict = get_schema("station", SCHEMA_FN_PATHS)
-attr_dict.add_dict(get_schema("fdsn", SCHEMA_FN_PATHS), "fdsn")
-location_dict = get_schema("location", SCHEMA_FN_PATHS)
-location_dict.add_dict(get_schema("declination", SCHEMA_FN_PATHS), "declination")
+attr_dict.add_dict(get_schema("fdsn", TS_SCHEMA_FN_PATHS), "fdsn")
+location_dict = get_schema("location", TS_SCHEMA_FN_PATHS)
+location_dict.add_dict(get_schema("declination", TS_SCHEMA_FN_PATHS), "declination")
 attr_dict.add_dict(location_dict, "location")
 attr_dict.add_dict(
-    get_schema("person", SCHEMA_FN_PATHS), "acquired_by", keys=["author", "comments"]
+    get_schema("person", TS_SCHEMA_FN_PATHS), "acquired_by", keys=["author", "comments"]
 )
-attr_dict.add_dict(get_schema("orientation", SCHEMA_FN_PATHS), "orientation")
+attr_dict.add_dict(get_schema("orientation", TS_SCHEMA_FN_PATHS), "orientation")
 attr_dict.add_dict(
-    get_schema("provenance", SCHEMA_FN_PATHS),
+    get_schema("provenance", TS_SCHEMA_FN_PATHS),
     "provenance",
     keys=["comments", "creation_time", "log"],
 )
-attr_dict.add_dict(get_schema("software", SCHEMA_FN_PATHS), "provenance.software")
+attr_dict.add_dict(get_schema("software", TS_SCHEMA_FN_PATHS), "provenance.software")
 attr_dict.add_dict(
-    get_schema("person", SCHEMA_FN_PATHS),
+    get_schema("person", TS_SCHEMA_FN_PATHS),
     "provenance.submitter",
     keys=["author", "email", "organization"],
 )
-attr_dict.add_dict(get_schema("time_period", SCHEMA_FN_PATHS), "time_period")
+attr_dict.add_dict(get_schema("time_period", TS_SCHEMA_FN_PATHS), "time_period")
 attr_dict.add_dict(
     get_schema("transfer_function", SCHEMA_FN_PATHS), "transfer_function"
 )
