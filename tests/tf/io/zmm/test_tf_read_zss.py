2ffa2c284b04844021d6e65fe22ff6fba384b588
JP <peacock.jared@gmail.com>
2022-03-30 10:57:36 -0700

    added zmm,zss to tf read

--- tests/tf/io/zmm/test_tf_read_zss.py
+++ tests/tf/io/zmm/test_tf_read_zss.py
@@ -12,7 +12,7 @@ import unittest
 
 import numpy as np
 from collections import OrderedDict
-from mt_metadata import TF_ZMM
+from mt_metadata import TF_ZSS_TIPPER
 from mt_metadata.transfer_functions.core import TF
 
 # =============================================================================
@@ -22,7 +22,7 @@ from mt_metadata.transfer_functions.core import TF
 
 class TestEMTFXML(unittest.TestCase):
     def setUp(self):
-        self.tf = TF(fn=TF_ZMM)
+        self.tf = TF(fn=TF_ZSS_TIPPER)
         self.maxDiff = None
 
     def test_survey_metadata(self):
@@ -48,16 +48,16 @@ class TestEMTFXML(unittest.TestCase):
         self.assertDictEqual(meta_dict, self.tf.survey_metadata.to_dict(single=True))
 
     def test_station_metadata(self):
-        meta_dict = OrderedDict([('channels_recorded', ['ex', 'ey', 'hx', 'hy', 'hz']),
-                     ('comments', 'WITH FULL ERROR COVARINCE'),
+        meta_dict = OrderedDict([('channels_recorded', ['hx', 'hy', 'hz']),
+                     ('comments', 'WITH FULL ERROR COVARIANCE'),
                      ('data_type', 'MT'),
                      ('geographic_name', None),
-                     ('id', '300'),
+                     ('id', 'YSW212abcdefghijkl'),
                      ('location.declination.model', 'WMM'),
-                     ('location.declination.value', 13.1),
+                     ('location.declination.value', 11.18),
                      ('location.elevation', 0.0),
-                     ('location.latitude', 34.727),
-                     ('location.longitude', -115.735),
+                     ('location.latitude', 44.631),
+                     ('location.longitude', -110.44),
                      ('orientation.method', None),
                      ('orientation.reference_frame', 'geographic'),
                      ('provenance.creation_time', '1980-01-01T00:00:00+00:00'),
@@ -66,15 +66,16 @@ class TestEMTFXML(unittest.TestCase):
                      ('provenance.software.version', '1'),
                      ('provenance.submitter.email', None),
                      ('provenance.submitter.organization', None),
-                     ('run_list', ['300a']),
+                     ('run_list', ['station    :ysw212abcdefghijkla']),
                      ('time_period.end', '1980-01-01T00:00:00+00:00'),
                      ('time_period.start', '1980-01-01T00:00:00+00:00'),
                      ('transfer_function.coordinate_system', 'geopgraphic'),
-                     ('transfer_function.id', '300'),
+                     ('transfer_function.id', 'station    :ysw212abcdefghijkl'),
                      ('transfer_function.processed_date', None),
                      ('transfer_function.processing_parameters', []),
                      ('transfer_function.remote_references', []),
-                     ('transfer_function.runs_processed', ['300a']),
+                     ('transfer_function.runs_processed',
+                      ['station    :ysw212abcdefghijkla']),
                      ('transfer_function.sign_convention', None),
                      ('transfer_function.units', None)])
 
@@ -82,7 +83,7 @@ class TestEMTFXML(unittest.TestCase):
 
     def test_run_metadata(self):
         meta_dict = OrderedDict([('channels_recorded_auxiliary', []),
-                     ('channels_recorded_electric', ['ex', 'ey']),
+                     ('channels_recorded_electric', []),
                      ('channels_recorded_magnetic', ['hx', 'hy', 'hz']),
                      ('data_logger.firmware.author', None),
                      ('data_logger.firmware.name', None),
@@ -94,70 +95,30 @@ class TestEMTFXML(unittest.TestCase):
                      ('data_logger.timing_system.uncertainty', 0.0),
                      ('data_logger.type', None),
                      ('data_type', 'BBMT'),
-                     ('id', '300a'),
-                     ('sample_rate', 8.0),
+                     ('id', 'station    :ysw212abcdefghijkla'),
+                     ('sample_rate', 5.0),
                      ('time_period.end', '1980-01-01T00:00:00+00:00'),
                      ('time_period.start', '1980-01-01T00:00:00+00:00')])
 
         self.assertDictEqual(meta_dict, self.tf.station_metadata.runs[0].to_dict(single=True))
 
     def test_z(self):
-        with self.subTest(msg="shape"):
-            self.assertTupleEqual((38, 2, 2), self.tf.impedance.shape)
             
-        with self.subTest('has impedance'):
-            self.assertTrue(self.tf.has_impedance())
-
-        with self.subTest(msg="first element"):
-            self.assertTrue(
-                np.isclose(
-                    self.tf.impedance[0],
-                    np.array([[ -5.99100018 -5.95499992j,  17.27000046+12.72000027j],
-                           [-51.59000015-23.03000069j,  -0.35179999 +7.66300011j]]),
-                ).all()
-            )
-
-        with self.subTest(msg="last element"):
-            self.assertTrue(
-                np.isclose(
-                    self.tf.impedance[-1],
-                    np.array([[  1.69399995e-04+0.01282j   ,   5.67400008e-02+0.06538j   ],
-                           [  3.78899992e-01-0.85600001j,   2.71499991e-01-0.4786j    ]]),
-                ).all()
-            )
-            
-    def test_z_err(self):
-        with self.subTest(msg="shape"):
-            self.assertTupleEqual((38, 2, 2), self.tf.impedance_error.shape)
-
-        with self.subTest(msg="first element"):
-            self.assertTrue(
-                np.isclose(
-                    self.tf.impedance_error[0],
-                    np.array([[ 0.53822149,  1.44624196],
-                           [ 1.92694986,  5.17786036]]),
-                ).all()
-            )
-
-        with self.subTest(msg="last element"):
-            self.assertTrue(
-                np.isclose(
-                    self.tf.impedance_error[-1],
-                    np.array([[ 0.00273188,  0.0020596 ],
-                           [ 1.12854018,  0.85081953]]),
-                ).all()
-            )
+        self.assertFalse(self.tf.has_impedance())
 
 
     def test_t(self):
+        with self.subTest("has tipper"):
+            self.assertTrue(self.tf.has_tipper())
+        
         with self.subTest(msg="shape"):
-            self.assertTupleEqual((38, 1, 2), self.tf.tipper.shape)
+            self.assertTupleEqual((44, 1, 2), self.tf.tipper.shape)
 
         with self.subTest(msg="first element"):
             self.assertTrue(
                 np.isclose(
                     self.tf.tipper[0],
-                    np.array([[ 0.25870001-0.18619999j, -0.05068000+0.0659j    ]]),
+                    np.array([[-0.20389999+0.09208j,  0.05996000+0.03177j]]),
                 ).all()
             )
 
@@ -165,13 +126,13 @@ class TestEMTFXML(unittest.TestCase):
             self.assertTrue(
                 np.isclose(
                     self.tf.tipper[-1],
-                    np.array([[ 0.07606+0.1051j ,  0.03506-0.04627j]]),
+                    np.array([[-0.01205-0.03873j, -0.09759-0.03014j]]),
                 ).all()
             )
         
     def test_sip(self):
         with self.subTest(msg="shape"):
-            self.assertTupleEqual((38, 2, 2), self.tf.inverse_signal_power.shape)
+            self.assertTupleEqual((44, 2, 2), self.tf.inverse_signal_power.shape)
             
         with self.subTest('has inverse_signal_power'):
             self.assertTrue(self.tf.has_inverse_signal_power())
@@ -180,8 +141,8 @@ class TestEMTFXML(unittest.TestCase):
             self.assertTrue(
                 np.isclose(
                     self.tf.inverse_signal_power[0],
-                    np.array([[  18.05999947 -4.46999991e-07j,  -27.14999962 +6.88899994e+00j],
-                           [ -27.14999962 +6.88899994e+00j,  130.39999390 +2.38400006e-07j]]),
+                    np.array([[ 136.19999695+0.j        ,   -9.60000038-5.32700014j],
+                           [  -9.60000038-5.32700014j,  336.10000610+0.j        ]]),
                 ).all()
             )
 
@@ -189,16 +150,16 @@ class TestEMTFXML(unittest.TestCase):
             self.assertTrue(
                 np.isclose(
                     self.tf.inverse_signal_power[-1],
-                    np.array([[  1.92300007e-07 -4.44100010e-16j,
-                              3.36799992e-08 -1.40400003e-08j],
-                           [  3.36799992e-08 -1.40400003e-08j,
-                              1.09299997e-07 +0.00000000e+00j]]),
+                    np.array([[  3.59999990e-06 +0.00000000e+00j,
+                              2.10999997e-06 -9.44800007e-08j],
+                           [  2.10999997e-06 -9.44800007e-08j,
+                              4.55500003e-06 +0.00000000e+00j]]),
                 ).all()
             )
         
     def test_residual(self):
         with self.subTest(msg="shape"):
-            self.assertTupleEqual((38, 3, 3), self.tf.residual_covariance.shape)
+            self.assertTupleEqual((44, 3, 3), self.tf.residual_covariance.shape)
             
         with self.subTest('has residual_covariance'):
             self.assertTrue(self.tf.has_residual_covariance())
@@ -207,12 +168,9 @@ class TestEMTFXML(unittest.TestCase):
             self.assertTrue(
                 np.isclose(
                     self.tf.residual_covariance[0],
-                    np.array([[  1.60399992e-02+0.j       ,   2.29300000e-02+0.005487j ,
-                             -6.59599973e-05-0.0002254j],
-                           [  2.29300000e-02-0.005487j ,   2.05599993e-01+0.j       ,
-                              1.45800004e-04-0.0009467j],
-                           [ -6.59599973e-05+0.0002254j,   1.45800004e-04+0.0009467j,
-                              8.14200030e-05+0.j       ]]),
+                    np.array([[  0.00000000e+00+0.j,   0.00000000e+00+0.j,   0.00000000e+00+0.j],
+                           [  0.00000000e+00+0.j,   0.00000000e+00+0.j,   0.00000000e+00+0.j],
+                           [  0.00000000e+00+0.j,   0.00000000e+00+0.j,   4.62099982e-18+0.j]]),
                 ).all()
             )
 
@@ -220,15 +178,9 @@ class TestEMTFXML(unittest.TestCase):
             self.assertTrue(
                 np.isclose(
                     self.tf.residual_covariance[-1],
-                    np.array([[  3.88100014e+01 +0.00000000e+00j,
-                             -1.31300000e+03 -2.84700000e+03j,
-                              1.56499996e+01 -1.21499996e+01j],
-                           [ -1.31300000e+03 +2.84700000e+03j,
-                              6.62300000e+06 +0.00000000e+00j,
-                              8.53800000e+03 +1.40900000e+04j],
-                           [  1.56499996e+01 +1.21499996e+01j,
-                              8.53800000e+03 -1.40900000e+04j,
-                              1.17700000e+03 +0.00000000e+00j]]),
+                    np.array([[  0.00000000+0.j,   0.00000000+0.j,   0.00000000+0.j],
+                           [  0.00000000+0.j,   0.00000000+0.j,   0.00000000+0.j],
+                           [  0.00000000+0.j,   0.00000000+0.j,  38.70000076+0.j]]),
                 ).all()
             )
 
