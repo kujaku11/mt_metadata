18e985fd42df373865dae2a51f8a19ac561995b8
JP <peacock.jared@gmail.com>
2023-02-24 16:10:06 -0800

    added aurora folder

---
+++ mt_metadata/transfer_functions/processing/fourier_coefficients/decimation.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 17 14:15:20 2022
+
+@author: jpeacock
+"""
+# =============================================================================
+# Imports
+# =============================================================================
+from mt_metadata.base.helpers import write_lines
+from mt_metadata.base import get_schema, Base
+from .standards import SCHEMA_FN_PATHS
+
+# =============================================================================
+attr_dict = get_schema("decimation", SCHEMA_FN_PATHS)
+# =============================================================================
+
+
+class Decimation(Base):
+    __doc__ = write_lines(attr_dict)
+
+    def __init__(self, **kwargs):
+
+        super().__init__(attr_dict=attr_dict, **kwargs)
