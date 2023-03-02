c641d408ae3150c9575dcb4215bbd86f36801a59
JP <peacock.jared@gmail.com>
2023-03-02 11:27:12 -0800

    updating fcs

--- mt_metadata/transfer_functions/processing/fourier_coefficients/fc.py
+++ mt_metadata/transfer_functions/processing/fourier_coefficients/fc.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Feb 17 14:15:20 2022
+Created on Fri Feb 25 15:20:59 2022
 
 @author: jpeacock
 """
@@ -9,14 +9,16 @@ Created on Thu Feb 17 14:15:20 2022
 # =============================================================================
 from mt_metadata.base.helpers import write_lines
 from mt_metadata.base import get_schema, Base
+from mt_metadata.timeseries import TimePeriod
 from .standards import SCHEMA_FN_PATHS
 
 # =============================================================================
-attr_dict = get_schema("decimation", SCHEMA_FN_PATHS)
+attr_dict = get_schema("fc", SCHEMA_FN_PATHS)
+attr_dict.add_dict(TimePeriod()._attr_dict)
+
+
 # =============================================================================
-
-
-class Decimation(Base):
+class FC(Base):
     __doc__ = write_lines(attr_dict)
 
     def __init__(self, **kwargs):
