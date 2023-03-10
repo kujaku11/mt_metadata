e375ff2df8ff4f42e4b1d2be1b016457de8e33bc
JP <peacock.jared@gmail.com>
2023-03-10 14:29:22 -0800

    Update test_tf_write.py

--- tests/tf/io/emtfxml/test_tf_write.py
+++ tests/tf/io/emtfxml/test_tf_write.py
@@ -29,10 +29,77 @@ class TestWriteEMTFXML(unittest.TestCase):
 
         self.x0 = EMTFXML(TF_XML)
 
-    def test_element_object(self):
-        for key in self.x1.element_keys:
-            with self.subTest(key):
-                self.assertEqual(getattr(self.x0, key), getattr(self.x1, key))
+    def test_description(self):
+        self.assertEqual(self.x0.description, self.x1.description)
+
+    def test_product_id(self):
+        self.assertEqual(self.x0.product_id, self.x1.product_id)
+
+    def test_sub_type(self):
+        self.assertEqual(self.x0.sub_type, self.x1.sub_type)
+
+    def test_notes(self):
+        self.assertEqual(self.x0.notes, self.x1.notes)
+
+    def test_tags(self):
+        self.assertListEqual(
+            [v.strip() for v in self.x0.tags.split(",")],
+            [v.strip() for v in self.x1.tags.split(",")],
+        )
+
+    def test_external_url(self):
+        with self.subTest("attribute"):
+            self.assertEqual(self.x0.external_url, self.x1.external_url)
+
+        with self.subTest("to_xml"):
+            self.assertEqual(
+                self.x0.external_url.to_xml(string=True),
+                self.x1.external_url.to_xml(string=True),
+            )
+
+    def test_primary_data(self):
+        with self.subTest("attribute"):
+            self.assertEqual(self.x0.primary_data, self.x1.primary_data)
+
+        with self.subTest("to_xml"):
+            self.assertEqual(
+                self.x0.primary_data.to_xml(string=True),
+                self.x1.primary_data.to_xml(string=True),
+            )
+
+    def test_attachment(self):
+        with self.subTest("attribute"):
+            self.assertEqual(self.x0.attachment, self.x1.attachment)
+
+        with self.subTest("to_xml"):
+            self.assertEqual(
+                self.x0.attachment.to_xml(string=True),
+                self.x1.attachment.to_xml(string=True),
+            )
+
+    def test_provenance(self):
+        d0 = self.x0.provenance.to_dict(single=True)
+        d1 = self.x1.provenance.to_dict(single=True)
+
+        for key in ["create_time", "creating_application"]:
+            d0.pop(key)
+            d1.pop(key)
+
+        self.assertDictEqual(d0, d1)
+
+    # def test_element_object(self):
+    #     for key in self.x1.element_keys:
+    #         with self.subTest(key):
+    #             if key in ["provenance", "statistical_estimates"]:
+    #                 self.assertNotEqual(
+    #                     getattr(self.x0, key), getattr(self.x1, key)
+    #                 )
+    #             elif key in ["tags"]:
+    #
+    #             else:
+    #                 self.assertEqual(
+    #                     getattr(self.x0, key), getattr(self.x1, key)
+    #                 )
 
 
 # =============================================================================
