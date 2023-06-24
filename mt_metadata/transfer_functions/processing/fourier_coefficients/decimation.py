48e91e2f4a9329882f5391bd3b98644b02895f74
JP <peacock.jared@gmail.com>
2023-06-23 17:22:41 -0700

    added listdict to fc metadata

--- mt_metadata/transfer_functions/processing/fourier_coefficients/decimation.py
+++ mt_metadata/transfer_functions/processing/fourier_coefficients/decimation.py
@@ -7,15 +7,20 @@ Created on Fri Feb 25 15:20:59 2022
 # =============================================================================
 # Imports
 # =============================================================================
+from collections import OrderedDict
 from mt_metadata.base.helpers import write_lines
 from mt_metadata.base import get_schema, Base
 from mt_metadata.timeseries import TimePeriod
 from mt_metadata.transfer_functions.processing.aurora import Window
+from mt_metadata.transfer_functions.processing.fourier_coefficients import (
+    Channel,
+)
 from .standards import SCHEMA_FN_PATHS
+from mt_metadata.utils.list_dict import ListDict
 
 # =============================================================================
 attr_dict = get_schema("decimation", SCHEMA_FN_PATHS)
-attr_dict.add_dict(TimePeriod()._attr_dict)
+attr_dict.add_dict(TimePeriod()._attr_dict, "time_period")
 attr_dict.add_dict(Window()._attr_dict, "window")
 
 # =============================================================================
@@ -24,21 +29,232 @@ class Decimation(Base):
 
     def __init__(self, **kwargs):
         self.window = Window()
-        self._time_period = TimePeriod()
+        self.time_period = TimePeriod()
+        self.channels = ListDict()
         super().__init__(attr_dict=attr_dict, **kwargs)
 
+    def __len__(self):
+        return len(self.channels)
+
+    def __add__(self, other):
+        if isinstance(other, Decimation):
+            self.channels.extend(other.channels)
+
+            return self
+        else:
+            msg = f"Can only merge ch objects, not {type(other)}"
+            self.logger.error(msg)
+            raise TypeError(msg)
+
+    def update(self, other, match=[]):
+        """
+        Update attribute values from another like element, skipping None
+
+        :param other: DESCRIPTION
+        :type other: TYPE
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+        if not isinstance(other, type(self)):
+            self.logger.warning(
+                "Cannot update %s with %s", type(self), type(other)
+            )
+        for k in match:
+            if self.get_attr_from_name(k) != other.get_attr_from_name(k):
+                msg = "%s is not equal %s != %s"
+                self.logger.error(
+                    msg,
+                    k,
+                    self.get_attr_from_name(k),
+                    other.get_attr_from_name(k),
+                )
+                raise ValueError(
+                    msg,
+                    k,
+                    self.get_attr_from_name(k),
+                    other.get_attr_from_name(k),
+                )
+        for k, v in other.to_dict(single=True).items():
+            if hasattr(v, "size"):
+                if v.size > 0:
+                    self.set_attr_from_name(k, v)
+            else:
+                if v not in [None, 0.0, [], "", "1980-01-01T00:00:00+00:00"]:
+                    self.set_attr_from_name(k, v)
+
+        ## Need this because channels are set when setting channels_recorded
+        ## and it initiates an empty channel, but we need to fill it with
+        ## the appropriate metadata.
+        for ch in other.channels:
+            self.add_channel(ch)
+
     @property
-    def start(self):
-        return self._time_period.start
+    def channels_estimated(self):
+        """channels for fcs were estimated"""
+        return [
+            ch.component
+            for ch in self.channels.values()
+            if ch.component is not None
+        ]
 
-    @start.setter
-    def start(self, start):
-        self._time_period.start = start
+    @channels_estimated.setter
+    def channels_estimated(self, value):
+        """set channels esimated"""
+
+        if value is None:
+            self.logger.debug("Input channel name is None, skipping")
+            return
+        if not hasattr(value, "__iter__"):
+            value = [value]
+
+        for entry in value:
+            if isinstance(entry, str):
+                self.add_channel(Channel(component=entry))
+            elif entry is None:
+                continue
+            elif isinstance(entry, Channel):
+                self.add_channel(entry)
+            else:
+                msg = f"entry must be a string or type FCChannel not {type(entry)}"
+                self.logger.error(msg)
+                raise ValueError(msg)
+
+    def has_channel(self, component):
+        """
+        Check to see if the channel already exists
+
+        :param component: channel component to look for
+        :type component: string
+        :return: True if found, False if not
+        :rtype: boolean
+
+        """
+
+        if component in self.channels_esimated:
+            return True
+        return False
+
+    def channel_index(self, component):
+        """
+        get index of the channel in the channel list
+        """
+        if self.has_channel(component):
+            return self.channels_estimated.index(component)
+        return None
+
+    def get_channel(self, component):
+        """
+        Get a channel
+
+        :param component: channel component to look for
+        :type component: string
+        :return: channel object based on channel type
+        :rtype: :class:`mt_metadata.timeseries.Channel`
+
+        """
+
+        if self.has_channel(component):
+            return self.channels[component]
+
+    def add_channel(self, channel_obj):
+        """
+        Add a channel to the list, check if one exists if it does overwrite it
+
+        :param channel_obj: channel object to add
+        :type channel_obj: :class:`mt_metadata.transfer_functions.processing.fourier_coefficients.Channel`
+
+        """
+        if not isinstance(channel_obj, (Channel)):
+            msg = f"Input must be metadata.Channel not {type(channel_obj)}"
+            self.logger.error(msg)
+            raise ValueError(msg)
+
+        if self.has_channel(channel_obj.component):
+            self.channels[channel_obj.component].update(channel_obj)
+            self.logger.debug(
+                f"ch {channel_obj.component} already exists, updating metadata"
+            )
+
+        else:
+            self.channels.append(channel_obj)
+
+    def remove_channel(self, channel_id):
+        """
+        remove a ch from the survey
+
+        :param component: channel component to look for
+        :type component: string
+
+        """
+
+        if self.has_channel(channel_id):
+            self.channels.remove(channel_id)
+        else:
+            self.logger.warning(f"Could not find {channel_id} to remove.")
 
     @property
-    def end(self):
-        return self._time_period.end
+    def channels(self):
+        """List of channels in the ch"""
+        return self._channels
 
-    @end.setter
-    def end(self, end):
-        self._time_period.end = end
+    @channels.setter
+    def channels(self, value):
+        """set the channel list"""
+
+        if not isinstance(value, (list, tuple, dict, ListDict, OrderedDict)):
+            msg = (
+                "input ch_list must be an iterable, should be a list or dict "
+                f"not {type(value)}"
+            )
+            self.logger.error(msg)
+            raise TypeError(msg)
+
+        fails = []
+        self._channels = ListDict()
+        if isinstance(value, (dict, ListDict, OrderedDict)):
+            value_list = value.values()
+
+        elif isinstance(value, (list, tuple)):
+            value_list = value
+
+        for ii, channel in enumerate(value_list):
+            try:
+                ch = Channel()
+                ch.from_dict(channel)
+                self._channels.append(ch)
+            except Exception as error:
+                msg = "Could not create channel from dictionary: %s"
+                fails.append(msg % error)
+                self.logger.error(msg, error)
+
+        if len(fails) > 0:
+            raise TypeError("\n".join(fails))
+
+    @property
+    def n_channels(self):
+        return self.__len__()
+
+    def update_time_period(self):
+        """
+        update time period from ch information
+        """
+        start = []
+        end = []
+        for ch in self.channels:
+            if ch.time_period.start != "1980-01-01T00:00:00+00:00":
+                start.append(ch.time_period.start)
+            if ch.time_period.start != "1980-01-01T00:00:00+00:00":
+                end.append(ch.time_period.end)
+        if start:
+            if self.time_period.start == "1980-01-01T00:00:00+00:00":
+                self.time_period.start = min(start)
+            else:
+                if self.time_period.start > min(start):
+                    self.time_period.start = min(start)
+        if end:
+            if self.time_period.end == "1980-01-01T00:00:00+00:00":
+                self.time_period.end = max(end)
+            else:
+                if self.time_period.end < max(end):
+                    self.time_period.end = max(end)
