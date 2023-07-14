5d7ceb7b9fb62b7b15d0bb2316335be9eb50b7a5
JP <peacock.jared@gmail.com>
2023-07-14 14:21:58 -0700

    Update notes.py

--- mt_metadata/base/notes.py
+++ mt_metadata/base/notes.py
@@ -8,8 +8,15 @@ Created on Fri Jul 14 10:35:45 2023
 # =============================================================================
 # Imports
 # =============================================================================
+from pydantic import BaseModel
 
 # =============================================================================
 # pydantic Notes
 # need to probably make each attribute a base class and then amalgamate them
 # because we are not just type checking but style checking as well?
+
+
+class Element(BaseModel, validate_assignment=True):
+    @classmethod
+    def parse_dict(cls, kw_dict):
+        return cls.create_model(kw_dict)
