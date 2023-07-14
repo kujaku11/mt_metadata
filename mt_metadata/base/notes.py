a6923f408551c52b5b60fca26c95de44f4e63892
JP <peacock.jared@gmail.com>
2023-07-14 10:38:11 -0700

    Create notes.py

---
+++ mt_metadata/base/notes.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 14 10:35:45 2023
+
+@author: jpeacock
+"""
+
+# =============================================================================
+# Imports
+# =============================================================================
+
+# =============================================================================
+# pydantic Notes
+# need to probably make each attribute a base class and then amalgamate them
+# because we are not just type checking but style checking as well?
