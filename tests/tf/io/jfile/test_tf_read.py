e011c157f619320262daf3c83500cd3617b1eba4
JP <peacock.jared@gmail.com>
2023-08-29 15:09:33 -0700

    Update test_tf_read.py

--- tests/tf/io/jfile/test_tf_read.py
+++ tests/tf/io/jfile/test_tf_read.py
@@ -80,7 +80,7 @@ class TestEMTFXML(unittest.TestCase):
                 ("time_period.start", "1980-01-01T00:00:00+00:00"),
                 ("transfer_function.coordinate_system", "geopgraphic"),
                 ("transfer_function.id", "BP05"),
-                ("transfer_function.processed_date", "1980-01-01"),
+                ("transfer_function.processed_date", "2023-04-21"),
                 (
                     "transfer_function.processing_parameters",
                     [
