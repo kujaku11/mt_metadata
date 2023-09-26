c6040025d1309149a0b89ca95e8822d44b3d2e1a
JP <peacock.jared@gmail.com>
2023-09-26 11:19:08 -0700

    updated tf test to use get_nm_elev for elevation

--- tests/tf/io/zonge/test_tf_read.py
+++ tests/tf/io/zonge/test_tf_read.py
@@ -14,6 +14,7 @@ import numpy as np
 from collections import OrderedDict
 from mt_metadata import TF_AVG
 from mt_metadata.transfer_functions import TF
+from mt_metadata.transfer_functions.io.tools import get_nm_elev
 
 # =============================================================================
 # EMTFXML
@@ -64,7 +65,10 @@ class TestAVG(unittest.TestCase):
                 ("location.datum", "WGS84"),
                 ("location.declination.model", "WMM"),
                 ("location.declination.value", 0.0),
-                ("location.elevation", 1414.37487793),
+                (
+                    "location.elevation",
+                    get_nm_elev(32.83331167, -107.08305667),
+                ),
                 ("location.latitude", 32.83331167),
                 ("location.longitude", -107.08305667),
                 ("orientation.method", None),
