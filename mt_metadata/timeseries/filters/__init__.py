9b4301848cf29a7d0670d6798c5dcfcc88f5524d
kkappler <magnetotellurics@gmail.com>
2023-12-08 18:01:59 -0800

    rename channel_response_filter.py to channel_response.py

--- mt_metadata/timeseries/filters/__init__.py
+++ mt_metadata/timeseries/filters/__init__.py
@@ -3,7 +3,7 @@ from .fir_filter import FIRFilter
 from .pole_zero_filter import PoleZeroFilter
 from .time_delay_filter import TimeDelayFilter
 from .frequency_response_table_filter import FrequencyResponseTableFilter
-from .channel_response_filter import ChannelResponseFilter
+from .channel_response import ChannelResponseFilter
 
 
 __all__ = [
