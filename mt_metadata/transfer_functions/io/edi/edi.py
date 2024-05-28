c1cb47ff49730fba070f2e861568e279a91309d8
JP <peacock.jared@gmail.com>
2024-05-28 14:23:39 -0700

    update how processing parameters are written.

--- mt_metadata/transfer_functions/io/edi/edi.py
+++ mt_metadata/transfer_functions/io/edi/edi.py
@@ -419,7 +419,8 @@ class EDI(object):
                         )
                 elif key.startswith("t"):
                     obj[:, ii, jj] = (
-                        data_dict[f"{key}r.exp"] + data_dict[f"{key}i.exp"] * 1j
+                        data_dict[f"{key}r.exp"]
+                        + data_dict[f"{key}i.exp"] * 1j
                     )
                     try:
                         error_key = [
@@ -756,7 +757,9 @@ class EDI(object):
                 f"\toriginal_program.date={self.Header.progdate}\n"
             )
         if self.Header.fileby != "1980-01-01":
-            extra_lines.append(f"\toriginal_file.date={self.Header.filedate}\n")
+            extra_lines.append(
+                f"\toriginal_file.date={self.Header.filedate}\n"
+            )
         header_lines = self.Header.write_header(
             longitude_format=longitude_format, latlon_format=latlon_format
         )
@@ -904,11 +907,15 @@ class EDI(object):
             ]
         elif data_key.lower() == "freq":
             block_lines = [
-                ">{0} // {1:.0f}\n".format(data_key.upper(), data_comp_arr.size)
+                ">{0} // {1:.0f}\n".format(
+                    data_key.upper(), data_comp_arr.size
+                )
             ]
         elif data_key.lower() in ["zrot", "trot"]:
             block_lines = [
-                ">{0} // {1:.0f}\n".format(data_key.upper(), data_comp_arr.size)
+                ">{0} // {1:.0f}\n".format(
+                    data_key.upper(), data_comp_arr.size
+                )
             ]
         else:
             raise ValueError("Cannot write block for {0}".format(data_key))
@@ -1196,7 +1203,7 @@ class EDI(object):
                 if k in ["processing_parameters"]:
                     for item in v:
                         self.Info.info_list.append(
-                            f"transfer_function.{item.replace('=', ' = ')}"
+                            f"transfer_function.processing_parameters.{item.replace('=', ' = ')}"
                         )
                 else:
                     self.Info.info_list.append(f"transfer_function.{k} = {v}")
