1c7504b4b6cfd837dc39a06c86b1dc4bf7bcd5f2
Karl N. Kappler <magnetotellurics@gmail.com>
2024-06-19 10:04:19 -0700

    Update numpy float convention
    MTH5 notebooks failing on python >=3.9 due to:
    AttributeError: `np.float_` was removed in the NumPy 2.0 release. Use `np.float64` instead
    - removed `np.float_` reference, float64 was already there

--- mt_metadata/utils/validators.py
+++ mt_metadata/utils/validators.py
@@ -378,6 +378,15 @@ def validate_default(value_dict):
 
 def validate_value_type(value, v_type, style=None):
     """
+
+    :param value:
+    :type value:
+    :param v_type:
+    :type v_type:
+    :param style:
+    :type style:
+    :return:
+
     validate type from standards
 
     """
@@ -469,7 +478,7 @@ def validate_value_type(value, v_type, style=None):
 
         # if a number convert to appropriate type
         elif isinstance(
-            value, (float, np.float_, np.float16, np.float32, np.float64)
+            value, (float, np.float16, np.float32, np.float64)
         ):
             if v_type is int:
                 return int(value)
