5332a63ea63bc415baf353ce7334e845460096fd
JP <peacock.jared@gmail.com>
2024-07-25 13:48:51 -0700

    Update emeasurement.py

--- mt_metadata/transfer_functions/io/edi/metadata/emeasurement.py
+++ mt_metadata/transfer_functions/io/edi/metadata/emeasurement.py
@@ -41,8 +41,9 @@ class EMeasurement(Base):
 
         super().__init__(attr_dict=attr_dict, **kwargs)
 
-        if self.x != 0 and self.y != 0 and self.x2 != 0 and self.y2 != 0:
-            self.azm = self.azimuth
+        if self.azm == 0:
+            if self.x != 0 or self.x2 != 0 or self.y != 0 or self.y2 != 0:
+                self.azm = self.azimuth
 
     def __str__(self):
         return "\n".join(
