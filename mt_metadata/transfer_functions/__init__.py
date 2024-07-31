43afc4d5c5631b87e53ff2238c448a3fc5bf4257
JP <peacock.jared@gmail.com>
2024-07-30 18:01:46 -0700

    Update __init__.py

--- mt_metadata/transfer_functions/__init__.py
+++ mt_metadata/transfer_functions/__init__.py
@@ -1,7 +1,3 @@
-from .core import TF
-
-__all__ = ["TF"]
-
 # Define allowed sets of channel labellings
 STANDARD_INPUT_CHANNELS = [
     "hx",
@@ -13,35 +9,13 @@ STANDARD_OUTPUT_CHANNELS = [
     "hz",
 ]
 
-channel_maps = {
+CHANNEL_MAPS = {
     "default": {"hx": "hx", "hy": "hy", "hz": "hz", "ex": "ex", "ey": "ey"},
     "lemi12": {"hx": "bx", "hy": "by", "hz": "bz", "ex": "e1", "ey": "e2"},
     "lemi34": {"hx": "bx", "hy": "by", "hz": "bz", "ex": "e3", "ey": "e4"},
     "phoenix123": {"hx": "h1", "hy": "h2", "hz": "h3", "ex": "e1", "ey": "e2"},
     "musgraves": {"hx": "bx", "hy": "by", "hz": "bz", "ex": "ex", "ey": "ey"},
 }
-
-
-def load_channel_maps():
-    """
-    :return: Keys are the channel_nomenclature schema keywords.
-     Values are dictionaries which map the STANDARD_INPUT_NAMES, \
-     STANDARD_OUTPUT_NAMES to the channel names associated with a given
-     channel nomenclature
-    :rtype: dict
-    """
-    import json
-    import pathlib
-
-    fn = pathlib.Path(__file__).parent.joinpath(
-        "standards", "channel_nomenclatures.json"
-    )
-    with open(fn) as f:
-        channel_maps = json.loads(f.read())
-    return channel_maps
-
-
-CHANNEL_MAPS = load_channel_maps()
 
 
 def get_allowed_channel_names(standard_names):
@@ -61,3 +35,7 @@ def get_allowed_channel_names(standard_names):
 
 ALLOWED_INPUT_CHANNELS = get_allowed_channel_names(STANDARD_INPUT_CHANNELS)
 ALLOWED_OUTPUT_CHANNELS = get_allowed_channel_names(STANDARD_OUTPUT_CHANNELS)
+
+from .core import TF
+
+__all__ = ["TF"]
