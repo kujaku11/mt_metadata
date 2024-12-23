6da352a601bdec77fdd2dd9c24e30ef1940149a1
kkappler <magnetotellurics@gmail.com>
2024-12-23 12:04:34 -0800

    add doc

--- mt_metadata/transfer_functions/processing/aurora/decimation.py
+++ mt_metadata/transfer_functions/processing/aurora/decimation.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 """
+This module contains the metadata Decimation class from aurora.
+
+TODO: consider renaming to AuroraDecimation to contrast with other Decimation objects.
+
 Created on Thu Feb 17 14:15:20 2022
 
 @author: jpeacock
@@ -17,6 +21,12 @@ attr_dict = get_schema("decimation", SCHEMA_FN_PATHS)
 
 
 class Decimation(Base):
+    """
+        The decimation class contains two key pieces of information:
+        1. The decimation level, an integer that tells the sequential order in a decimation scheme.
+        2. The decimation factor.  This is normally an integer, but the decimation.json does allow for floating point values.
+
+    """
     __doc__ = write_lines(attr_dict)
 
     def __init__(self, **kwargs):
