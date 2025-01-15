31ab84a99da3abfca9b507189cbe335e5ce83a1f
Karl N. Kappler <magnetotellurics@gmail.com>
2025-01-15 10:39:07 -0800

    update tests for inf in metadata float and str

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -16,6 +16,8 @@ Created on Tue Apr 28 18:08:40 2020
 # =============================================================================
 
 import unittest
+
+import numpy as np
 from mt_metadata.base import Base
 from mt_metadata.utils.exceptions import MTValidatorError
 
@@ -86,12 +88,12 @@ class TestBase(unittest.TestCase):
         with self.subTest("float"):
             number_list = [10, "11", 12.6, "13.3", "-inf"]
             self.assertEqual(
-                [10.0, 11.0, 12.6, 13.3, "inf"],
+                [10.0, 11.0, 12.6, 13.3, -np.inf],
                 self.base_object._validate_type(number_list, float),
             )
         with self.subTest("string"):
             self.assertEqual(
-                ["10", "11", "12.6", "13.3"],
+                ["10", "11", "12.6", "13.3", "-inf"],
                 self.base_object._validate_type(number_list, str),
             )
         with self.subTest("bool"):
