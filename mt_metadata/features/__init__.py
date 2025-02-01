452ecf7f587ad6cd715ab5b2b8939af1ac23de18
JP <peacock.jared@gmail.com>
2025-01-31 16:59:54 -0800

    adding ts and fc features, got to decimation level

--- mt_metadata/features/__init__.py
+++ mt_metadata/features/__init__.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from .feature_ts_run import FeatureTSRun
+from .feature_fc_run import FeatureFCRun
 from .feature import Feature
 
-__all__ = ["Feature"]
+
+__all__ = ["FeatureTSRun", "FeatureFCRun", "Feature"]
--- mt_metadata/features/feature_fc_run.py
+++ mt_metadata/features/feature_fc_run.py
@@ -15,12 +15,12 @@ from .standards import SCHEMA_FN_PATHS
 from mt_metadata.timeseries import TimePeriod
 
 # =============================================================================
-attr_dict = get_schema("feature_ts_run", SCHEMA_FN_PATHS)
+attr_dict = get_schema("feature_fc_run", SCHEMA_FN_PATHS)
 attr_dict.add_dict(TimePeriod()._attr_dict, "time_period")
 
 
 # =============================================================================
-class FeatureTSRun(Base):
+class FeatureFCRun(Base):
     __doc__ = write_lines(attr_dict)
 
     def __init__(self, **kwargs):
