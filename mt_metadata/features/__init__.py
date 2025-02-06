cc7e45e0560ed3fb3af05b965769053dcb9d907f
JP <peacock.jared@gmail.com>
2025-02-06 14:51:08 -0800

    updating features

--- mt_metadata/features/__init__.py
+++ mt_metadata/features/__init__.py
@@ -2,7 +2,13 @@
 
 from .feature_ts_run import FeatureTSRun
 from .feature_fc_run import FeatureFCRun
+from .feature_decimation_channel import FeatureDecimationChannel
 from .feature import Feature
 
 
-__all__ = ["FeatureTSRun", "FeatureFCRun", "Feature"]
+__all__ = [
+    "FeatureTSRun",
+    "FeatureFCRun",
+    "Feature",
+    "FeatureDecimationChannel",
+]
