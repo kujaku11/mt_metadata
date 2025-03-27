711168443de780ed5fc364a423cdbbbd135d114b
kkappler <magnetotellurics@gmail.com>
2025-03-27 06:50:13 -0700

    move get_degenerate_fc_decimation from mth5 to mt_metadata

--- mt_metadata/transfer_functions/processing/fourier_coefficients/decimation.py
+++ mt_metadata/transfer_functions/processing/fourier_coefficients/decimation.py
@@ -418,3 +418,33 @@ def fc_decimations_creator(
         fc_decimations.append(fc_dec)
 
     return fc_decimations
+
+def get_degenerate_fc_decimation(sample_rate: float) -> list:
+    """
+        WIP
+
+        Makes a default fc_decimation list.
+        This "degenerate" config will only operate on the first decimation level.
+        This is useful for testing.  It could also be used in future on an MTH5 stored
+        time series in decimation levels already as separate runs.
+
+    Parameters
+    ----------
+    sample_rate: float
+        The sample rate associated with the time-series to convert to spectrogram
+
+    Returns
+    -------
+    output: list
+        List has only one element which is of type FCDecimation, aka.
+
+    """
+    output = fc_decimations_creator(
+        sample_rate,
+        decimation_factors=[
+            1,
+        ],
+        max_levels=1,
+    )
+    return output
+
