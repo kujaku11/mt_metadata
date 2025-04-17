96c809aaefd7b5abe0ed037cea32975a567f06cf
JP <peacock.jared@gmail.com>
2025-04-16 17:03:27 -0700

    updated copyright basemodel

---
+++ mt_metadata/utils/create_license_list.py
@@ -0,0 +1,46 @@
+"""
+Create a list of licenses using the SPDX license list.
+This script fetches the SPDX license list from the SPDX GitHub repository
+and creates a JSON file containing the license IDs and their corresponding names.
+
+https://github.com/spdx/license-list-data/blob/main/json/licenses.json
+
+"""
+
+# =============================================================================
+# Imports
+# =============================================================================
+from enum import Enum
+import json
+
+# ==============================================================================
+
+# Load the licenses JSON file
+filename = r"c:\Users\peaco\Downloads\licenses.json"
+with open(filename, "r") as fid:
+    licenses = json.load(fid)
+
+# Process the license IDs to make them valid Python identifiers
+license_variable_list = []
+for license in licenses["licenses"]:
+    item = {}
+    item["license_key"] = (
+        license["licenseId"]
+        .replace("-", "_")
+        .replace(" ", "_")
+        .replace(".", "_")
+        .replace("(", "_")
+        .replace(")", "_")
+        .replace("/", "_")
+        .replace(":", "_")
+    )
+    item["license_value"] = license["licenseId"]
+    license_variable_list.append(item)
+
+# Create an Enum class for the licenses
+LicenseEnum = Enum(
+    "LicenseEnum",  # Name of the Enum
+    {
+        entry["license_key"]: entry["license_value"] for entry in license_variable_list
+    },  # Members
+)
