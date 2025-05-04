b34920c04bfd3a76b9287abe90cc71e9ea860b4b
Karl N. Kappler <magnetotellurics@gmail.com>
2025-05-03 17:25:49 -0700

    update feature class

--- mt_metadata/features/feature.py
+++ mt_metadata/features/feature.py
@@ -13,13 +13,34 @@ from mt_metadata.base import get_schema, Base
 from .standards import SCHEMA_FN_PATHS
 
 # =============================================================================
-attr_dict = get_schema("feature", SCHEMA_FN_PATHS)
+# attr_dict = get_schema("feature", SCHEMA_FN_PATHS)
 
+from mt_metadata.features.base_feature import BaseFeature
+
+def _make_supported_features_dict():
+    # TODO Import all supported fetaures here
+    from mt_metadata.features.coherence import Coherence
+    SUPPORTED_FEATURE_DICT = {}
+    SUPPORTED_FEATURE_DICT["coherence"] = Coherence
+    return SUPPORTED_FEATURE_DICT
 
 # =============================================================================
-class Feature(Base):
-    __doc__ = write_lines(attr_dict)
+class Feature(BaseFeature):
+    # __doc__ = write_lines(attr_dict)
 
     def __init__(self, **kwargs):
 
-        super().__init__(attr_dict=attr_dict, **kwargs)
+        super().__init__(**kwargs)
+        self._supported_features = _make_supported_features_dict()
+
+    def from_dict(self, input_dict):
+        if "name" not in input_dict.keys():
+            msg = f"Features must have an ID (name), supported features are {SUPPORTED_FEATURE_DICT.keys()}"
+
+        feature = SUPPORTED_FEATURE_DICT[input_dict.pop("feature_name")]
+        feature_obj = feature()
+        feature_obj.from_dict(input_dict)
+        print("yay")
+
+
+
