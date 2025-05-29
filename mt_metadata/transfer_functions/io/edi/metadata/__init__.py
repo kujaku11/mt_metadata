21c144692db97ad85d5bdb35d53681770b692136
JP <peacock.jared@gmail.com>
2025-05-28 23:24:26 -0700

    updating define_measurement

--- mt_metadata/transfer_functions/io/edi/metadata/__init__.py
+++ mt_metadata/transfer_functions/io/edi/metadata/__init__.py
@@ -1,19 +1,22 @@
 # package file
 
 
-# from .emeasurement import EMeasurement
-# from .hmeasurement import HMeasurement
+from .emeasurement import EMeasurement
+from .hmeasurement import HMeasurement
+
 # from .information import Information
-# from .data_section import DataSection
-# from .define_measurement import DefineMeasurement
+from .data_section import DataSection
+
+from .define_measurement import DefineMeasurement
+
 # from .header import Header
 
 
-# __all__ = [
-#     "Header",
-#     "Information",
-#     "HMeasurement",
-#     "EMeasurement",
-#     "DefineMeasurement",
-#     "DataSection",
-# ]
+__all__ = [
+    #     "Header",
+    #     "Information",
+    "HMeasurement",
+    "EMeasurement",
+    "DefineMeasurement",
+    "DataSection",
+]
--- mt_metadata/transfer_functions/io/edi/metadata/emeasurement.py
+++ mt_metadata/transfer_functions/io/edi/metadata/emeasurement.py
@@ -1,33 +1,167 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Dec  4 16:21:06 2021
-
-@author: jpeacock
-"""
-
-# =============================================================================
+# =====================================================
 # Imports
-# =============================================================================
+# =====================================================
+from typing import Annotated
+
 import numpy as np
+from pydantic import (
+    computed_field,
+    Field,
+    field_validator,
+    model_validator,
+    PrivateAttr,
+)
 
-from mt_metadata.base import Base, get_schema
-from mt_metadata.base.helpers import write_lines
-
-from .standards import SCHEMA_FN_PATHS
+from mt_metadata.base import MetadataBase
 
 
-# =============================================================================
-attr_dict = get_schema("emeasurement", SCHEMA_FN_PATHS)
+# =====================================================
 
 
-# ==============================================================================
-# magnetic measurements
-# ==============================================================================
-class EMeasurement(Base):
-    __doc__ = write_lines(attr_dict)
+class EMeasurement(MetadataBase):
+    id: Annotated[
+        float | None,
+        Field(
+            default=0.0,
+            description="Channel number, could be location.channel_number.",
+            examples=["1"],
+            alias=None,
+            json_schema_extra={
+                "units": None,
+                "required": True,
+            },
+        ),
+    ]
 
-    def __init__(self, **kwargs):
-        self._fmt_dict = {
+    chtype: Annotated[
+        str,
+        Field(
+            default="",
+            description="channel type, should start with an 'e'",
+            examples=["ex"],
+            alias=None,
+            pattern=r"^[eE][a-zA-Z0-9_]+$",
+            json_schema_extra={
+                "units": None,
+                "required": True,
+            },
+        ),
+    ]
+
+    x: Annotated[
+        float,
+        Field(
+            default=0.0,
+            description="location of negative sensor relative center point in north direction",
+            examples=["100.0"],
+            alias=None,
+            json_schema_extra={
+                "units": "meters",
+                "required": True,
+            },
+        ),
+    ]
+
+    x2: Annotated[
+        float,
+        Field(
+            default=0.0,
+            description="location of positive sensor relative center point in north direction",
+            examples=["100.0"],
+            alias=None,
+            json_schema_extra={
+                "units": "meters",
+                "required": True,
+            },
+        ),
+    ]
+
+    y: Annotated[
+        float,
+        Field(
+            default=0.0,
+            description="location of negative sensor relative center point in east direction",
+            examples=["100.0"],
+            alias=None,
+            json_schema_extra={
+                "units": "meters",
+                "required": True,
+            },
+        ),
+    ]
+
+    y2: Annotated[
+        float,
+        Field(
+            default=0.0,
+            description="location of positive sensor relative center point in east direction",
+            examples=["100.0"],
+            alias=None,
+            json_schema_extra={
+                "units": "meters",
+                "required": True,
+            },
+        ),
+    ]
+
+    z: Annotated[
+        float,
+        Field(
+            default=0.0,
+            description="location of negative sensor relative center point in depth",
+            examples=["100.0"],
+            alias=None,
+            json_schema_extra={
+                "units": "meters",
+                "required": True,
+            },
+        ),
+    ]
+
+    z2: Annotated[
+        float,
+        Field(
+            default=0.0,
+            description="location of positive sensor relative center point in depth",
+            examples=["100.0"],
+            alias=None,
+            json_schema_extra={
+                "units": "meters",
+                "required": True,
+            },
+        ),
+    ]
+
+    azm: Annotated[
+        float,
+        Field(
+            default=0.0,
+            description="orientation of the sensor relative to coordinate system, clockwise positive.",
+            examples=["100.0"],
+            alias=None,
+            json_schema_extra={
+                "units": "degrees",
+                "required": True,
+            },
+        ),
+    ]
+
+    acqchan: Annotated[
+        str,
+        Field(
+            default="",
+            description="description of acquired channel",
+            examples=["100.0"],
+            alias=None,
+            json_schema_extra={
+                "units": None,
+                "required": True,
+            },
+        ),
+    ]
+
+    _fmt_dict: dict[str, str] = PrivateAttr(
+        default={
             "id": "<",
             "chtype": "<",
             "x": "<.2f",
@@ -39,12 +173,33 @@ class EMeasurement(Base):
             "azm": "<.2f",
             "acqchan": "<",
         }
+    )
 
-        super().__init__(attr_dict=attr_dict, **kwargs)
+    @field_validator("id", mode="before")
+    @classmethod
+    def validate_id(cls, value: float | str | None) -> float:
+        """Ensure id is a float or None, convert if necessary"""
+        if isinstance(value, str):
+            try:
+                value = float(value)
+            except ValueError:
+                raise ValueError("id must be a number or convertible to float")
+        elif not isinstance(value, (float, int, type(None))):
+            raise TypeError("id must be a number or None")
+        if value is None:
+            value = 0.0  # Default to 0.0 if None
+        return value
 
-        if self.azm == 0:
-            if self.x != 0 or self.x2 != 0 or self.y != 0 or self.y2 != 0:
-                self.azm = self.azimuth
+    @model_validator(mode="after")
+    def update_azimuth_from_coords(self):
+        """Update azm based on coordinates after validation"""
+        # Only update if coordinates have been explicitly set
+        if any(value != 0 for value in [self.x, self.y, self.x2, self.y2]):
+            computed_azimuth = self.azimuth
+            if self.azm == 0:  # Only update if azm wasn't explicitly set
+                # Bypass validation by using object.__setattr__
+                object.__setattr__(self, "azm", computed_azimuth)
+        return self
 
     def __str__(self):
         return "\n".join([f"{k} = {v}" for k, v in self.to_dict(single=True).items()])
@@ -52,34 +207,39 @@ class EMeasurement(Base):
     def __repr__(self):
         return self.__str__()
 
+    @computed_field
     @property
-    def dipole_length(self):
+    def dipole_length(self) -> float:
+        """dipole length based on x, y, z coordinates"""
         try:
-            if hasattr(self, "z"):
-                return np.sqrt(
-                    (self.x2 - self.x) ** 2 + (self.y2 - self.y) ** 2 + self.z**2
-                )
-            else:
-                return np.sqrt((self.x2 - self.x) ** 2 + (self.y2 - self.y) ** 2)
+            return (
+                (self.x2 - self.x) ** 2
+                + (self.y2 - self.y) ** 2
+                + (self.z2 - self.z) ** 2
+            ) ** 0.5
         except TypeError:
             return 0
 
+    @computed_field
     @property
-    def azimuth(self):
+    def azimuth(self) -> float:
+        """aximuth based on x, y coordinates"""
         try:
             return np.rad2deg(np.arctan2((self.y2 - self.y), (self.x2 - self.x)))
         except (ZeroDivisionError, TypeError):
             return 0.0
 
+    @computed_field
     @property
-    def channel_number(self):
-        if self.acqchan != None:
+    def channel_number(self) -> int:
+        """Extract channel number from acqchan."""
+        if self.acqchan is not None:
             if not isinstance(self.acqchan, (int, float)):
                 try:
-                    return [int("".join(i for i in self.acqchan if i.isdigit()))][0]
+                    return int("".join(i for i in self.acqchan if i.isdigit()))
                 except (IndexError, ValueError):
                     return 0
-            return self.acqchan
+            return int(self.acqchan)
         return 0
 
     def write_meas_line(self):
