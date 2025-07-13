17f2fd178468ebcdddd74776a6013a341963849a
JP <peacock.jared@gmail.com>
2025-07-13 14:47:44 -0700

    updating data type warnings and a test

--- mt_metadata/transfer_functions/io/emtfxml/metadata/data_quality_warnings_basemodel.py
+++ mt_metadata/transfer_functions/io/emtfxml/metadata/data_quality_warnings_basemodel.py
@@ -3,9 +3,12 @@
 # =====================================================
 from typing import Annotated
 
-from pydantic import Field
+from pydantic import Field, field_validator
 
 from mt_metadata.base import MetadataBase
+from mt_metadata.transfer_functions.io.emtfxml.metadata import helpers
+
+from .comment_basemodel import Comment
 
 
 # =====================================================
@@ -23,3 +26,59 @@ class DataQualityWarnings(MetadataBase):
             },
         ),
     ]
+    comments: Annotated[
+        Comment | None,
+        Field(
+            default_factory=Comment,  # type: ignore
+            description="Comments about the data quality warnings",
+            examples=["Data quality is good", "Some issues found"],
+            alias=None,
+            json_schema_extra={
+                "units": None,
+                "required": False,
+            },
+        ),
+    ]
+
+    @field_validator("comments", mode="before")
+    @classmethod
+    def validate_comments(cls, v):
+        """Ensure comments is an instance of Comment."""
+        if isinstance(v, Comment):
+            return v
+        elif isinstance(v, str):
+            return Comment(value=v)  # type: ignore
+        elif v is None:
+            return Comment()  # type: ignore
+        else:
+            raise TypeError("comments must be a Comment instance or a string")
+
+    def read_dict(self, input_dict):
+        """
+
+        :param input_dict: DESCRIPTION
+        :type input_dict: TYPE
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+        helpers._read_element(self, input_dict, "data_quality_warnings")
+
+    def to_xml(self, string=False, required=True):
+        """
+
+        :param string: DESCRIPTION, defaults to False
+        :type string: TYPE, optional
+        :param required: DESCRIPTION, defaults to True
+        :type required: TYPE, optional
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+
+        return helpers.to_xml(
+            self,
+            string=string,
+            required=required,
+            order=["flag", "comments"],
+        )
