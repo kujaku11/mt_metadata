e30ae1c445f5ecffb1cd946a9bd50c54cf4bec48
JP <peacock.jared@gmail.com>
2025-07-18 14:29:25 -0700

    merging with main to get the fc, processing files

--- mt_metadata/processing/aurora/decimation.py
+++ mt_metadata/processing/aurora/decimation.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Feb 17 14:15:20 2022
+Created on Fri Jan 31 13:39:39 2025
 
 @author: jpeacock
 """
+<<<<<<<< HEAD:mt_metadata/processing/aurora/decimation.py
 from mt_metadata.base import Base, get_schema
+========
+>>>>>>>> main:mt_metadata/features/feature_fc_run.py
 
 # =============================================================================
 # Imports
@@ -13,14 +16,24 @@ from mt_metadata.base.helpers import write_lines
 
 from .standards import SCHEMA_FN_PATHS
 
+<<<<<<<< HEAD:mt_metadata/processing/aurora/decimation.py
+========
+from mt_metadata.timeseries import TimePeriod
+>>>>>>>> main:mt_metadata/features/feature_fc_run.py
 
 # =============================================================================
-attr_dict = get_schema("decimation", SCHEMA_FN_PATHS)
+attr_dict = get_schema("feature_fc_run", SCHEMA_FN_PATHS)
+attr_dict.add_dict(TimePeriod()._attr_dict, "time_period")
+
+
 # =============================================================================
-
-
-class Decimation(Base):
+class FeatureFCRun(Base):
     __doc__ = write_lines(attr_dict)
 
     def __init__(self, **kwargs):
+<<<<<<<< HEAD:mt_metadata/processing/aurora/decimation.py
+========
+
+        self.time_period = TimePeriod()
+>>>>>>>> main:mt_metadata/features/feature_fc_run.py
         super().__init__(attr_dict=attr_dict, **kwargs)
