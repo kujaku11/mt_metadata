1aa320db28481eeb15c9c5a43b1e7b3f1b45ad23
JP <peacock.jared@gmail.com>
2025-08-17 17:01:54 -0700

    Update channel_basemodel.py

--- mt_metadata/processing/aurora/channel_basemodel.py
+++ mt_metadata/processing/aurora/channel_basemodel.py
@@ -1,28 +1,39 @@
-#=====================================================
+# =====================================================
 # Imports
-#=====================================================
+# =====================================================
 from typing import Annotated
 
-from mt_metadata.base import MetadataBase
 from pydantic import Field
 
+from mt_metadata.base import MetadataBase
 
-#=====================================================
+
+# =====================================================
 class Channel(MetadataBase):
-    id: Annotated[str, Field(
-    default=",
-    description='channel ID',
-    examples=['mt001'],
-    alias=None,
-    json_schema_extra={'units':None,'required':True,},
+    id: Annotated[
+        str,
+        Field(
+            default="",
+            description="channel ID",
+            examples=["mt001"],
+            alias=None,
+            json_schema_extra={
+                "units": None,
+                "required": True,
+            },
+        ),
+    ]
 
-    )]
-
-    scale_factor: Annotated[float, Field(
-    default=1.0,
-    description='scale factor of the channel',
-    examples=['10.0'],
-    alias=None,
-    json_schema_extra={'units':None,'required':True,},
-
-    )]
+    scale_factor: Annotated[
+        float,
+        Field(
+            default=1.0,
+            description="scale factor of the channel",
+            examples=["10.0"],
+            alias=None,
+            json_schema_extra={
+                "units": None,
+                "required": True,
+            },
+        ),
+    ]
