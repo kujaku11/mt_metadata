d036640c38dd046f2db519cc8036b63d75b1496c
JP <peacock.jared@gmail.com>
2025-08-17 17:01:32 -0700

    Update config_basemodel.py

--- mt_metadata/processing/aurora/config_basemodel.py
+++ mt_metadata/processing/aurora/config_basemodel.py
@@ -1,37 +1,53 @@
-#=====================================================
+# =====================================================
 # Imports
-#=====================================================
+# =====================================================
 from typing import Annotated
 
-from mt_metadata.base import MetadataBase
 from pydantic import Field
 
+from mt_metadata.base import MetadataBase
 
-#=====================================================
+
+# =====================================================
 class Config(MetadataBase):
-    config_id: Annotated[str, Field(
-    default=",
-    description='ID of the configuration setup. ',
-    examples=['CAS04-01'],
-    alias=None,
-    json_schema_extra={'units':None,'required':True,},
+    config_id: Annotated[
+        str,
+        Field(
+            default="",
+            description="ID of the configuration setup. ",
+            examples=["CAS04-01"],
+            alias=None,
+            json_schema_extra={
+                "units": None,
+                "required": True,
+            },
+        ),
+    ]
 
-    )]
+    longitude: Annotated[
+        float,
+        Field(
+            default=0.0,
+            description="Longitude of location in datum specified at survey level.",
+            examples=["14.23"],
+            alias=["lon", "long"],
+            json_schema_extra={
+                "units": "degrees",
+                "required": True,
+            },
+        ),
+    ]
 
-    longitude: Annotated[float, Field(
-    default=0.0,
-    description='Longitude of location in datum specified at survey level.',
-    examples=['14.23'],
-    alias=['lon', 'long'],
-    json_schema_extra={'units':'degrees','required':True,},
-
-    )]
-
-    elevation: Annotated[float, Field(
-    default=0.0,
-    description='Elevation of location in datum specified at survey level.',
-    examples=['123.4'],
-    alias=['elev'],
-    json_schema_extra={'units':'meters','required':True,},
-
-    )]
+    elevation: Annotated[
+        float,
+        Field(
+            default=0.0,
+            description="Elevation of location in datum specified at survey level.",
+            examples=["123.4"],
+            alias=["elev"],
+            json_schema_extra={
+                "units": "meters",
+                "required": True,
+            },
+        ),
+    ]
