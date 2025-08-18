ed504344d0ff91101369bdf00067a90c113db2e2
JP <peacock.jared@gmail.com>
2025-08-17 17:02:23 -0700

    Update decimation_basemodel.py

--- mt_metadata/processing/fourier_coefficients/decimation_basemodel.py
+++ mt_metadata/processing/fourier_coefficients/decimation_basemodel.py
@@ -1,29 +1,40 @@
-#=====================================================
+# =====================================================
 # Imports
-#=====================================================
+# =====================================================
 from typing import Annotated
 
-from mt_metadata.base import MetadataBase
 from pydantic import Field
 
+from mt_metadata.base import MetadataBase
 
-#=====================================================
+
+# =====================================================
 class Decimation(MetadataBase):
-    id: Annotated[str, Field(
-    default=",
-    description='Decimation level ID',
-    examples=['1'],
-    alias=None,
-    json_schema_extra={'units':None,'required':True,},
+    id: Annotated[
+        str,
+        Field(
+            default="",
+            description="Decimation level ID",
+            examples=["1"],
+            alias=None,
+            json_schema_extra={
+                "units": None,
+                "required": True,
+            },
+        ),
+    ]
 
-    )]
-
-    channels_estimated: Annotated[str, Field(
-    default='[]',
-    items={'type': 'string'},
-    description='list of channels',
-    examples=['[ex, hy]'],
-    alias=None,
-    json_schema_extra={'units':None,'required':True,},
-
-    )]
+    channels_estimated: Annotated[
+        str,
+        Field(
+            default="[]",
+            items={"type": "string"},
+            description="list of channels",
+            examples=["[ex, hy]"],
+            alias=None,
+            json_schema_extra={
+                "units": None,
+                "required": True,
+            },
+        ),
+    ]
