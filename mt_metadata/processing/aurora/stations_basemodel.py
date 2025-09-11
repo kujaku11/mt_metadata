3470ca9d6f91f116e1f1b030060123b4bd89ea84
JP <peacock.jared@gmail.com>
2025-09-11 00:01:05 -0700

    Update stations_basemodel.py

--- mt_metadata/processing/aurora/stations_basemodel.py
+++ mt_metadata/processing/aurora/stations_basemodel.py
@@ -3,18 +3,33 @@
 # =====================================================
 from typing import Annotated
 
-from mt_metadata.base import MetadataBase
 from pydantic import Field
+
+from mt_metadata.base import MetadataBase
+from mt_metadata.processing.aurora.station_basemodel import Station
 
 
 # =====================================================
 class Stations(MetadataBase):
     remote: Annotated[
-        str,
+        list[Station],
         Field(
-            default="[]",
-            items={"type": "string"},
+            default_factory=list,
             description="list of remote sites",
+            examples=["10"],
+            alias=None,
+            json_schema_extra={
+                "units": None,
+                "required": True,
+            },
+        ),
+    ]
+
+    local: Annotated[
+        Station,
+        Field(
+            default_factory=Station,  # type: ignore
+            description="local site",
             examples=["10"],
             alias=None,
             json_schema_extra={
