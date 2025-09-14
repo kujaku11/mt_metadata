ba7dc7e13de062aa16d53c04e3bc9a206f9e3484
JP <peacock.jared@gmail.com>
2025-09-13 21:00:10 -0700

    moved test_band to common

--- mt_metadata/base/__init__.py
+++ mt_metadata/base/__init__.py
@@ -2,7 +2,6 @@
 Base classes for holding metadata and schema objects
 """
 
-from .metadata import MetadataBase, Base
-from .schema import get_schema, BaseDict
+from .metadata import MetadataBase
 
-__all__ = ["MetadataBase", "Base", "get_schema", "BaseDict"]
+__all__ = ["MetadataBase"]
